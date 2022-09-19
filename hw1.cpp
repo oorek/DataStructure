0be6358b75fcefe667106e65ad29df8678a795ab
user <user@oorek.local>
2022-09-19 23:24:29 +0900

    hw1 done , hw2 needs revise

---
+++ hw1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void print(char *input, bool * check){
+    int len = strlen(input);
+    for(int i=0; i<len; i++){
+        cout << input[i];
+        if(check[i]){
+            cout << "/";
+        }
+    }
+    cout << "\n";
+}
+void backtracking(char * input, bool * check, int cur, bool tmp){
+    if(cur < 0){
+        if(tmp){
+            print(input, check);
+        }
+        return;
+    }
+    if(tmp){
+       check[cur] = true; 
+    }
+    backtracking(input, check, cur-1, false);
+    backtracking(input, check, cur-1, true);
+    check[cur] = false;
+}
+int main(){
+    char input[11];
+    bool check[10];
+    for(int i=0; i<10; i++)
+        check[i] = false;
+    
+    cin >> input;
+    int len = strlen(input);
+   
+    input[len] = '\0';
+    
+    if(len == 1)
+        cout << input << endl;
+    else{
+        backtracking(input, check, len-2, false);
+        backtracking(input, check, len-2, true);
+    }
+    return 0;
+}
---
+++ hw2.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+//bool success;
+void print(char * input, char * check){
+    int len = strlen(input);
+    for(int i=0; i<len-1; i++){
+        cout << input[i];
+        if(check[i] == ' ')
+            continue;
+        cout << check[i];
+    }
+    cout << input[len-1] << endl;
+}
+void find_star(char * check, bool *idx, int len){
+    for(int i=0; i<len; i++){
+        if(check[i] == '*')
+            idx[i] = true;
+    }
+}
+void concat_blank(int *arr, char * input, char * check){
+    int len = strlen(input);
+    for(int i=0; i<len;i++){
+        arr[i] = input[i] - 48;
+    }
+    for(int i=0; i<len-1; i++){
+        if(check[i] == ' '){
+            int tmp = arr[i]*10 + arr[i+1];
+            arr[i] = 0;
+            arr[i+1] = tmp;
+        }
+    }
+    // [1, 0, 23, 0, 45]
+}
+bool calculate(char * input, char * check, int target){
+    int arr[11];
+    // concat blank
+    concat_blank(arr, input, check);
+
+    // mult first
+    int len = strlen(input);
+    for(int i=0; i<len-1; i++){
+        if(check[i] == '*'){
+            int j=i+1;
+            while(j < len-1){
+                if(arr[j] != 0)
+                    break;
+                j++;
+            }
+            arr[j] = arr[i] * arr[j];
+            arr[i] = 0;
+        }
+    }
+    for(int i=0; i<len-1; i++){
+        if(check[i] == '+'){
+            int j=i+1;
+            while(j < len-1){
+                if(arr[j] != 0)
+                    break;
+                j++;
+            }
+            arr[j] = arr[i] + arr[j];
+            arr[i] = 0;
+        }
+        if(check[i] == '-'){
+            int j=i+1;
+            while(j < len-1){
+                if(arr[j] != 0)
+                    break;
+                j++;
+            }
+            arr[j] = arr[i] - arr[j];
+            arr[i] = 0;
+        }
+    }
+    if(arr[len-1] == target)
+        return true;
+    else
+        return false;
+}
+void backtracking(char *input, char  * symbol, char * check, int cur, int target){
+    //cout << *tmp << endl;
+    if(cur < 0){
+        if(calculate(input, check, target)){
+            print(input, check);
+            return;
+        }
+        return;
+    }
+    for(int i=0; i<4; i++){
+        check[cur] = symbol[i];
+        backtracking(input, symbol, check, cur-1, target);
+    }
+    check[cur] = 0;
+    
+}
+int main(){
+    char symbol[4] = {'+', '-', '*', ' '};
+    char input[11];
+    char check[10];
+    int target;
+    int *tmp = NULL;
+
+    cin >> input >> target;
+    int len = strlen(input);
+    input[len] = '\0';
+    
+    if(len == 1){
+        if(input[0]-48 == target)
+            cout << target << endl;
+        else
+            cout << "None" << endl;
+    }
+    else{
+        int tmp=0;
+        backtracking(input, symbol, check, len-2, target);
+        if(!tmp)
+            cout << "None" << endl;
+    }
+
+return 0;
+}
